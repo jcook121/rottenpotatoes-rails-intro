feca8be5f647ca7316636c4ce5f7ef4795de6a0a
Joseph Cook <jacoby746@yahoo.com>
2016-10-26 23:36:10 +0000

    Part 2 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,52 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.order(params[:sort])
     @sort_column = params[:sort]
-    @all_ratings = Movie.all_ratings
+    
+    @all_ratings= Movie.all_ratings
+    
+    unless params[:ratings].nil?
+     @filtered_ratings = params[:ratings].keys
+     @filtered_ratings = params[:ratings]
+     session[:filtered_ratings] = @filtered_ratings
+    end
+
+    if params[:sort].nil?
+      
+    else
+      session[:sort] = params[:sort]
+    end
+    
+    if params[:sort].nil? && params[:ratings].nil? && session[:filtered_ratings]
+      @filtered_ratings = session[:filtered_ratings]
+      @sort = session[:sort]
+
+      flash.keep
+      redirect_to movies_path({order_by: @sort, ratings: @filtered_ratings})
+    end
+
+    if session[:filtered_ratings]
+      @movies = @movies.select{ |movie| session[:filtered_ratings].include? movie.rating }
+    end
+ 
+    if params[:sort] == "title"
+      @movies = Movie.order("title asc")
+    if session[:sort] == "title"
+      @movies = @movies.sort { |a,b| a.title <=> b.title }
+
+       @movie_highlight = "hilite"
+    elsif params[:sort] == "release_date"
+      @movies = Movie.order("release_date asc")
+    elsif session[:sort] == "release_date"
+      @movies = @movies.sort! { |a,b| a.release_date <=> b.release_date }
+
+       @date_highlight = "hilite"
+    else
+      @movies = Movie.all
+    end
+ 
+    # Deal with movie ratings now...
+    @all_ratings = Movie.all_ratings()
+    end
   end
 
   def new
